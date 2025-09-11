e5bf5b2cf9f825210b92ab9e70d432eb0f094d32
EduMartinSilva <emaildoeduardoms@gmail.com>
2025-09-11 20:12:58 -0300

    continuando tentando colocar React

--- meu-portfolio-react/src/App.jsx
+++ meu-portfolio-react/src/App.jsx
@@ -1,7 +1,14 @@
 import React, { useEffect, useState } from "react";
 import './assets/style.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 
 function App() {
+ useEffect(() => {
+    AOS.init();
+  }, []);
+  
   const linhas = [
     "Olá, eu sou o Eduardo",
     "Seja bem vindo ao meu portfólio!",
@@ -35,54 +42,74 @@ function App() {
     }
   }, [linha, char, linhas]);
 
+  function App() {
+    return (
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+      >
+        <h1>Bem-vindo ao meu portfólio!</h1>
+      </motion.div>
+    );
+  }
+
   return (
-     <>
-      <header>
-        <div className="logo">Eduardo Dev</div>
-        <nav>
-          <ul>
-            <li><a href="">Skills / Tecnologias</a></li>
-            <li><a href="">Projetos</a></li>
-            <li><a href="">Contato</a></li>
-          </ul>
-        </nav>
-      </header>
+    <>
+      <div className="container" data-aos="fade-up">
+        <header>
+          <div className="logo">Eduardo Dev</div>
+          <nav>
+            <ul>
+              <li><a href="">Skills / Tecnologias</a></li>
+              <li><a href="">Projetos</a></li>
+              <li><a href="">Contato</a></li>
+            </ul>
+          </nav>
+        </header>
 
-      <div className="imagePerfil">
-        <img src="/assets/perfil.jpg" alt="Perfil" />
-      </div>
+        <div className="imagePerfil">
+          <img src="/assets/perfil.jpg" alt="Perfil" />
+        </div>
 
-      <div
-        id="welcome"
-        dangerouslySetInnerHTML={{ __html: texto }}
-      />
-      <div className="AboutMe">
-        <h1>Sobre mim</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus viverra lectus urna, in tempus felis
-          malesuada vitae. Mauris ultricies mauris commodo semper tempor. Etiam posuere volutpat odio ac tincidunt.
-          Aenean sed auctor felis, eget interdum magna. In cursus dictum neque, aliquet molestie ex ultrices ut.
-          Curabitur rutrum nisi non turpis consectetur, ut placerat urna semper. Donec dui orci, porta vel dolor sed,
-          suscipit feugiat neque. Fusce sed metus bibendum, volutpat mi sit amet, laoreet neque. Vivamus at felis
-          vulputate, ultrices dolor at, porttitor felis. Suspendisse consectetur turpis quis venenatis pretium. Mauris
-          vehicula pharetra ipsum. Phasellus luctus, quam et sollicitudin gravida, lectus lacus consequat tortor, et
-          sollicitudin odio urna vel ligula. Quisque id viverra risus. Vestibulum et justo fringilla, rutrum nulla
-          nec, aliquet leo. Duis condimentum augue lacus, ut facilisis urna vehicula nec.
-        </p>
-      </div>
-      <h1>Skills / Tecnologias</h1>
-      <div id="Skills">
-        <img src="/assets/python-icon.svg" alt="Python Logo" />
-        <img src="/assets/html5-icon.svg" alt="HTML Logo" />
-        <img src="/assets/css3-icon.svg" alt="CSS Logo" />
-        <img src="/assets/javascript-icon.svg" alt="JavaScript Logo" />
-        <img src="/assets/mysql-icon.svg" alt="MySQL Logo" />
-        <img src="/assets/git-icon.svg" alt="Git Logo" />
-        <img src="/assets/nodejs-icon.svg" alt="Node Logo" />
-        <img src="/assets/bootstrap-icon.svg" alt="Bootstrap Logo" />
-      </div>
-      <h1 id="title-projects">Projetos</h1>
+        <div
+          id="welcome"
+          dangerouslySetInnerHTML={{ __html: texto }}
+        />
+        <div className="AboutMe">
+          <h1>Sobre mim</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus viverra lectus urna, in tempus felis
+            malesuada vitae. Mauris ultricies mauris commodo semper tempor. Etiam posuere volutpat odio ac tincidunt.
+            Aenean sed auctor felis, eget interdum magna. In cursus dictum neque, aliquet molestie ex ultrices ut.
+            Curabitur rutrum nisi non turpis consectetur, ut placerat urna semper. Donec dui orci, porta vel dolor sed,
+            suscipit feugiat neque. Fusce sed metus bibendum, volutpat mi sit amet, laoreet neque. Vivamus at felis
+            vulputate, ultrices dolor at, porttitor felis. Suspendisse consectetur turpis quis venenatis pretium. Mauris
+            vehicula pharetra ipsum. Phasellus luctus, quam et sollicitudin gravida, lectus lacus consequat tortor, et
+            sollicitudin odio urna vel ligula. Quisque id viverra risus. Vestibulum et justo fringilla, rutrum nulla
+            nec, aliquet leo. Duis condimentum augue lacus, ut facilisis urna vehicula nec.
+          </p>
+        </div>
+        <h1>Skills / Tecnologias</h1>
+        <div id="Skills">
+          <img src="/assets/python-icon.svg" alt="Python Logo" />
+          <img src="/assets/html5-icon.svg" alt="HTML Logo" />
+          <img src="/assets/css3-icon.svg" alt="CSS Logo" />
+          <img src="/assets/javascript-icon.svg" alt="JavaScript Logo" />
+          <img src="/assets/mysql-icon.svg" alt="MySQL Logo" />
+          <img src="/assets/git-icon.svg" alt="Git Logo" />
+          <img src="/assets/nodejs-icon.svg" alt="Node Logo" />
+          <img src="/assets/bootstrap-icon.svg" alt="Bootstrap Logo" />
+        </div>
+        <h1 id="title-projects">Projetos</h1>
+
+        <footer>
+          <p>© 2024 Eduardo Dev. Todos os direitos reservados.</p>
+        </footer>
+      </div >
     </>
   )
 }
 export default App
+
+
